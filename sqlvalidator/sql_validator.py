789c92893f7740b77ef107cce1257bee7dfa62be
David-Wobrock <david.wobrock@gmail.com>
2021-03-22 19:51:27 +0100

    Add --validate option

--- sqlvalidator/sql_validator.py
+++ sqlvalidator/sql_validator.py
@@ -7,8 +7,18 @@ from sqlvalidator.grammar.tokeniser import to_tokens
 class SQLQuery:
     def __init__(self, sql: str):
         self.sql = sql
+        self._sql_query = None
         self.validated = False
         self.errors: List[str] = []
+
+    @property
+    def sql_query(self):
+        if self._sql_query is None:
+            self._sql_query = SQLStatementParser.parse(to_tokens(self.sql))
+        return self._sql_query
+
+    def format(self) -> str:
+        return self.sql_query.transform()
 
     def is_valid(self) -> bool:
         if not self.validated:
@@ -18,8 +28,7 @@ class SQLQuery:
     def _validate(self):
         self.validated = True
         try:
-            select_statement = SQLStatementParser.parse(to_tokens(self.sql))
-            self.errors = select_statement.validate()
+            self.errors = self.sql_query.validate()
         except ParsingError as ex:
             self.errors.append(str(ex))
 
--- tests/integration/test_file_handler.py
+++ tests/integration/test_file_handler.py
@@ -1,12 +1,12 @@
 from io import StringIO
 
-from sqlvalidator import file_formatter
+from sqlvalidator import file_handler
 
 
 def test_format_file():
     file_content = "'''select id from table_stmt;'''"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     expected_file_content = "'''\nSELECT id\nFROM table_stmt;\n'''"
     assert num_changed_sql == 1
     assert new_content == expected_file_content
@@ -15,7 +15,7 @@ def test_format_file():
 def test_format_file_multilines():
     file_content = "'select id from table_stmt'"
     input_file = StringIO(file_content)
-    _, new_content = file_formatter.get_formatted_file_content(input_file)
+    _, new_content, _ = file_handler.compute_file_content(input_file)
     expected_file_content = "'''\nSELECT id\nFROM table_stmt\n'''"
     assert new_content == expected_file_content
 
@@ -23,7 +23,7 @@ def test_format_file_multilines():
 def test_format_file_multiline_sql():
     file_content = "'''\nselect\n   id  \n   from table_stmt\n;\n'''"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     expected_file_content = "'''\nSELECT id\nFROM table_stmt;\n'''"
     assert num_changed_sql == 1
     assert new_content == expected_file_content
@@ -32,7 +32,7 @@ def test_format_file_multiline_sql():
 def test_format_file_multiline_sql_with_prefix():
     file_content = "r'''\nselect\n   id  \n   from table_stmt\n;\n'''"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     expected_file_content = "r'''\nSELECT id\nFROM table_stmt;\n'''"
     assert num_changed_sql == 1
     assert new_content == expected_file_content
@@ -41,7 +41,7 @@ def test_format_file_multiline_sql_with_prefix():
 def test_format_sql_string_with_strformat_call():
     file_content = "'select id from {}'.format(__file__)"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     expected_file_content = "'''\nSELECT id\nFROM {}\n'''.format(__file__)"
     assert num_changed_sql == 1
     assert new_content == expected_file_content
@@ -50,7 +50,7 @@ def test_format_sql_string_with_strformat_call():
 def test_no_format_file():
     file_content = "'select id from t'  # nosqlformat"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     assert num_changed_sql == 0
     assert new_content == file_content
 
@@ -58,7 +58,7 @@ def test_no_format_file():
 def test_no_format_sql_string_with_strformat_call():
     file_content = "'select id from {}'.format(__file__)  # nosqlformat"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     assert num_changed_sql == 0
     assert new_content == file_content
 
@@ -66,7 +66,7 @@ def test_no_format_sql_string_with_strformat_call():
 def test_two_following_statements():
     file_content = "sql = ('select * from t;', 'select count(*) from t;')"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     expected_file_content = (
         "sql = ('''\nSELECT *\nFROM t;\n''', '''\nSELECT COUNT(*)\nFROM t;\n''')"
     )
@@ -79,7 +79,7 @@ def test_two_following_statements_second_noformat():
         "sql = (\n'select * from t;', 'select count(*) from t;'  # nosqlformat\n)"
     )
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     expected_file_content = "sql = (\n'''\nSELECT *\nFROM t;\n''', 'select count(*) from t;'  # nosqlformat\n)"  # noqa
     assert num_changed_sql == 1
     assert new_content == expected_file_content
@@ -88,7 +88,7 @@ def test_two_following_statements_second_noformat():
 def test_two_following_statements_both_no_format():
     file_content = "sql = (\n'select * from t;'  # nosqlformat\n, 'select count(*) from t;'  # nosqlformat\n)"  # noqa
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     assert num_changed_sql == 0
     assert new_content == file_content
 
@@ -96,6 +96,6 @@ def test_two_following_statements_both_no_format():
 def test_string_starting_with_selected():
     file_content = "'selected string'"
     input_file = StringIO(file_content)
-    num_changed_sql, new_content = file_formatter.get_formatted_file_content(input_file)
+    num_changed_sql, new_content, _ = file_handler.compute_file_content(input_file)
     assert num_changed_sql == 0
     assert new_content == file_content
